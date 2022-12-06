428c81fd71daaa0c98beb0cca06765ce260fb3e5
Caleb Aregbesola <calebaregbesola@gmail.com>
2022-12-06 20:26:57 +0100

    WalletApp--ft--TransactionEntity

--- Wallet.Models/Entities/Transaction.cs
+++ Wallet.Models/Entities/Transaction.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WalletApp.Models.Entities
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum Type
+    {
+        Debit, Credit
+    }
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public Type Type { get; set; }
+        public int WalletId { get; set; }
+        public double Amount { get; set; }
+        public double Balance { get; set; }
+        public Wallet Wallet { get; set; }
+    }
+}
