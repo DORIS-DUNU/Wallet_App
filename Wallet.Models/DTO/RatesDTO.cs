44d9abc983701dfda1a9ca5ce49b60d8dafcbf17
Caleb Aregbesola <calebaregbesola@gmail.com>
2022-12-06 17:24:32 +0100

    WalletApp--ft--ConvertCurrency, RatesDTO

--- Wallet.Models/DTO/RatesDTO.cs
+++ Wallet.Models/DTO/RatesDTO.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalletApp.Models.DTO
+{
+    public class RatesDTO
+    {
+        public string result { get; set; }
+        public string documentation { get; set; }
+        public string terms_of_use { get; set; }
+        public int time_last_update_unix { get; set; }
+        public string time_last_update_utc { get; set; }
+        public int time_next_update_unix { get; set; }
+        public string time_next_update_utc { get; set; }
+        public string base_code { get; set; }
+        public Dictionary<string, double> rates { get; set; }
+    }
+}
