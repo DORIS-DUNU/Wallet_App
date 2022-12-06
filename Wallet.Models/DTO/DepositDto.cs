a07ee5f6ce51c8b301eeb26297ea28ba7aea4ad8
softimi <softimideji@gmail.com>
2022-12-06 17:27:16 +0100

    Wallet-fb-DepositDto

--- Wallet.Models/DTO/DepositDto.cs
+++ Wallet.Models/DTO/DepositDto.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalletApp.Models.DTO
+{
+    public class DepositDto
+    {
+        public double Amount { get; set; }
+        public string Address { get; set; }
+    }
+}
