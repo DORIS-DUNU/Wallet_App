4f3d62a6044031cc2c49f321bdef135ef01edc69
Caleb Aregbesola <calebaregbesola@gmail.com>
2022-12-06 21:24:43 +0100

    WalletApp--ft--TransactionServiceAbstraction

--- WalletApp.Abstractions/Services/ITransactionService.cs
+++ WalletApp.Abstractions/Services/ITransactionService.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WalletApp.Models.DTO;
+using WalletApp.Models.Entities;
+
+namespace WalletApp.Abstractions.Services
+{
+    public interface ITransactionService
+    {
+        public Task<double?> ConvertCurrencyAsync(string currencyA, string currencyB, double amount);
+        public Task<double?> GetRateAsync(string currencyCode, double? amount);
+
+        public Task<IEnumerable<TransactionDTO>> GetAllUserTransactionsAsync();
+        public Task<IEnumerable<TransactionDTO>> GetWalletStatementAsync(string walletAddress, int page);
+
+    }
+}
