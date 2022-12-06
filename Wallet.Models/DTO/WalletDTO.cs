5056074d24d5b54598acc59d053a7181d74da7f3
sam-adroit <samuelbankole779@gmail.com>
2022-12-06 17:23:10 +0100

    Created the Transfer and Wallet DTOs

--- Wallet.Models/DTO/WalletDTO.cs
+++ Wallet.Models/DTO/WalletDTO.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WalletApp.Models.Entities;
+
+namespace WalletApp.Models.DTO
+{
+    public class WalletDTO
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Address { get; set; }
+        //public string SecurityKey { get; set; }
+        public double Balance { get; set; }
+        // public ICollection<Transaction> Transactions { get; set; }
+    }
+}
