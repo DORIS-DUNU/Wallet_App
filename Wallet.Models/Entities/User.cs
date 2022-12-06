df7c476ab7adc913f7f82ca11e1746b00e1af00f
DORIS-DUNU <dunudoris3@gmail.com>
2022-12-06 20:33:25 +0100

    Wallet-ft-User Model

--- Wallet.Models/Entities/User.cs
+++ Wallet.Models/Entities/User.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WalletApp.Models.Entities
+{
+    public class User:IdentityUser
+    {
+        public string Name { get; set; }
+        public ICollection<Wallet> Wallets { get; set; }
+    }
+}
