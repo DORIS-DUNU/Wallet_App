e4749d0a4f3461fbbd07cf16496c9df2c112a57b
DORIS-DUNU <dunudoris3@gmail.com>
2022-12-06 17:32:37 +0100

    Created UserDTO

--- Wallet.Models/DTO/UserDTO.cs
+++ Wallet.Models/DTO/UserDTO.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalletApp.Models.DTO
+{
+    public class UserDTO
+
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
