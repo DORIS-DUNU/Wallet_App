035c982f92a7c0179ffc6f3f8ee5cb0398cb370f
Ekene Obiasogu <ekennyobiasogu@gmail.com>
2022-12-06 22:36:26 +0100

    walletApp-ft-login controller action

--- WalletApp/Controllers/UserController.cs
+++ WalletApp/Controllers/UserController.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WalletApp.Abstractions.Services;
+using WalletApp.Models.DTO;
+
+namespace WalletApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(IUserService userService, ILogger<UserController> logger)
+        {
+            _logger = logger;
+            _userService = userService;
+        }
+
+       
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var token = await _userService.LoginAsync(model);
+            if (token != null) return Ok(token);
+
+            _logger.LogInformation("Log in failed!");
+            return Problem();
+        }
+
+
+
+    }
+}
