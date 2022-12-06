1a775b269af05448e7694ef3753b5c1f5b354370
DORIS-DUNU <dunudoris3@gmail.com>
2022-12-06 22:08:20 +0100

    Wallet-ft-User Service

--- Wallet.Services/UserService.cs
+++ Wallet.Services/UserService.cs
@@ -1,1 +1,88 @@
-﻿
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WalletApp.Abstractions.Services;
+using WalletApp.Models.DTO;
+using WalletApp.Models.Entities;
+
+namespace WalletApp.Services
+{
+    public class UserService:IUserService
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public UserService(UserManager<User> userManager, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _userManager = userManager;
+        }
+
+        public async Task<String> Register(UserDTO user)
+        {
+
+            User newUser = new User()
+            {
+                Email = user.Email,
+                Name = user.Name,
+                UserName = user.Username
+            };
+            
+            var result = await _userManager.CreateAsync(newUser, user.Password);
+            if (result.Succeeded)
+                return "Successful";
+
+            return "Unsuccessful";
+
+        }
+
+
+
+        public async Task<object> LoginAsync(LoginModel model)
+        {
+            JwtSecurityToken token = null;
+            var user = await _userManager.FindByNameAsync(model.Username);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(ClaimTypes.NameIdentifier, user.Id),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+
+                foreach (var userRole in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                }
+
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+                token = new JwtSecurityToken(
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddHours(3),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
+            }
+            else
+            {
+                return "Wrong Credential";
+            }
+
+            return new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token),
+                expiration = token.ValidTo
+            };
+
+        }
+
+    }
+}
