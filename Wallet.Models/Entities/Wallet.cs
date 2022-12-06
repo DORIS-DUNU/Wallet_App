fefabcd3e90081ce2dd308b41adceedc22ed0bd1
sam-adroit <samuelbankole779@gmail.com>
2022-12-06 20:35:00 +0100

    Added wallet-entities

--- Wallet.Models/Entities/Wallet.cs
+++ Wallet.Models/Entities/Wallet.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalletApp.Models.Entities
+{
+    public class Wallet
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Address { get; set; }
+        public byte[] AddressHash { get; set; }
+        public byte[] AddressKey { get; set; }
+        public double Balance { get; set; }
+        public ICollection<Transaction> Transactions { get; set; }
+        public User User { get; set; }
+    }
+}
