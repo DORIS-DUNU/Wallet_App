f29ea112d548bcfec54560c7f191dbcd19794f46
Caleb Aregbesola <calebaregbesola@gmail.com>
2022-12-06 22:39:08 +0100

    WalletApp--ft--TransactionController

--- WalletApp/Controllers/TransactionController.cs
+++ WalletApp/Controllers/TransactionController.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WalletApp.Abstractions.Repositories;
+using WalletApp.Abstractions.Services;
+using WalletApp.Models.DTO;
+
+namespace WalletApp.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly ILogger<TransactionController> _logger;
+        private readonly ITransactionService _transactionService;
+
+        public TransactionController(ILogger<TransactionController> logger, ITransactionService transactionService)
+        {
+            _logger = logger;
+            _transactionService = transactionService;
+        }
+    }
+
+}
