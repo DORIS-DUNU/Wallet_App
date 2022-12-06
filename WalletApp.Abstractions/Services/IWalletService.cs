149a44b90d43af44d0abca774886e82f20c9836b
softimi <softimideji@gmail.com>
2022-12-06 21:30:35 +0100

    Wallet-fb-IWalletService

--- WalletApp.Abstractions/Services/IWalletService.cs
+++ WalletApp.Abstractions/Services/IWalletService.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using WalletApp.Models.DTO;
+
+namespace WalletApp.Abstractions.Services
+{
+    public interface IWalletService
+    {
+        public Task<string> CreateWalletAsync();
+        public Task<bool> DepositAsync(DepositDto deposit);
+        public Task<bool> WithdrawAsync(DepositDto withdraw);
+        public Task<bool> TransferAsync(TransferDto tranfer);
+        public Task<double?> GetBalanceAsync(string walletAddress, string currencyCode);
+        public Task<IEnumerable<WalletDTO>> GetListOfWallets();
+
+        //public Task<IEnumerable<TransactionDTO>> GetListOfTransactions()
+
+    }
+}
